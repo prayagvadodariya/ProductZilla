fd1d9afed6d101ba81b124fec1d821bdccf5874b
unknown <prayagvadodariya@gmail.com>
2021-06-14 19:59:09 +0530

    add drawer

--- src/component/Sidemenu.js
+++ src/component/Sidemenu.js
@@ -1,9 +1,10 @@
 import React, {useState, Component} from 'react';
 import { View, StyleSheet } from 'react-native';
-import { Layout, Text, Button, Toggle, useTheme } from '@ui-kitten/components';
+import { IndexPath, Menu, MenuItem, Toggle, useTheme } from '@ui-kitten/components';
 import { ThemeContext } from '../constant/ThemeContext';
 
 const Sidemenu = () => {
+  const [selectedIndex, setSelectedIndex] = React.useState(new IndexPath(0));
   const [checked, setChecked] = React.useState(false);
   const themeContext = React.useContext(ThemeContext);
   const theme = useTheme();
@@ -16,11 +17,23 @@ const Sidemenu = () => {
   };
   
     return (
-      <View style={styles.drawerContent}>
-        <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <View style={[styles.drawerContent, { backgroundColor: theme['background-basic-color-1'] }]}>
+        {/* <View>
+          <Text style={styles.sectionHeadingStyle1}>Test Dev</Text>
+          <Text style={styles.sectionHeadingStyle2}>testdev301@gmail.com</Text>
+        </View> */}
+        <Menu
+          selectedIndex={selectedIndex}
+          onSelect={index => setSelectedIndex(index)}>
+          <MenuItem title='Users'/>
+          <MenuItem title='Orders'/>
+          <MenuItem title='Transactions'/>
+          <MenuItem title='Settings'/>
+        </Menu>
+        {/* <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
           <Text category='h1'>HOME</Text>
         <Toggle checked={checked} onChange={onCheckedChange}></Toggle>
-      </Layout>
+      </Layout> */}
       </View>
     );
   }
@@ -28,9 +41,23 @@ const Sidemenu = () => {
 
 const styles = StyleSheet.create({
   drawerContent: {
-  flex: 1,
-  paddingTop: Platform.OS === 'android' ? 25 : 22,
- }
+    flex: 1,
+    paddingTop: Platform.OS === 'android' ? 25 : 22,
+  },
+  sectionHeadingStyle1: {
+    marginTop:3,
+    marginLeft:15,
+    fontSize:23,
+    color: 'gray',
+    fontWeight: 'bold'
+  },
+  sectionHeadingStyle2: {
+    marginTop:3,
+    marginLeft:15,
+    fontSize:17,
+    marginBottom:10,
+    color: 'gray',
+  },
 });
 
 export default Sidemenu;
