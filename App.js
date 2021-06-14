713f1c166871c40505f71f9f1975e4ae28d2d797
unknown <prayagvadodariya@gmail.com>
2021-06-14 13:01:41 +0530

    add

--- App.js
+++ App.js
@@ -3,26 +3,36 @@ import * as eva from '@eva-design/eva';
 import { ApplicationProvider, Layout, Text, Toggle } from '@ui-kitten/components';
 import { ThemeContext } from './src/constant/ThemeContext';
 import HomeScreen from './src/screens/Home';
-import { default as theme } from './src/constant/theme.json';
+import { default as lightTheme  } from './src/constant/light_theme.json';
+import { default as darkTheme } from './src/constant/dark_theme.json';
 
 export default () => {
 
-  // const [theme, setTheme] = React.useState('light');
+  const [theme, setTheme] = React.useState('light');
+  const [iscustomTheme,setCustomeTheme] = React.useState(lightTheme);
+ 
+  
 
-  // const toggleTheme = () => {
-  //   const nextTheme = theme === 'light' ? 'dark' : 'light';
-  //   setTheme(nextTheme);
-  // };
+  const toggleTheme = () => {
+    const nextTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(nextTheme);
+    if(nextTheme==='light'){
+      setCustomeTheme(lightTheme);
+    }else{
+      setCustomeTheme(darkTheme);
+    }
+  };
 
   return (
     // <ThemeContext.Provider value={{ theme, toggleTheme }}>
-    //   <ApplicationProvider {...eva} theme={eva[theme]}>
+    //   <ApplicationProvider {...eva} theme={{ ...eva[theme], ...iscustomTheme }}>
     //     <HomeScreen/>
     //   </ApplicationProvider>
-    // </ThemeContext.Provider>
-      <ApplicationProvider {...eva} theme={{ ...eva.light, ...theme }}>
+    // </ThemeContext.Provider>  
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <ApplicationProvider {...eva} theme={eva[theme]}>
         <HomeScreen/>
-      </ApplicationProvider>
-      
+       </ApplicationProvider>
+   </ThemeContext.Provider> 
   );
 };
