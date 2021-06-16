4d85fbba1ed7d0f54e5f938f63c7f599c9483478
unknown <prayagvadodariya@gmail.com>
2021-06-16 18:33:03 +0530

    shop design compoenent

--- src/component/Product_Content.js
+++ src/component/Product_Content.js
@@ -1,29 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, ImageBackground, FlatList } from 'react-native';
 import Colors from '../constant/Colors';
 import Htext from '../component/Htext';
+import * as StaticData from '../constant/StaticData';
+
  const Product_Content = (props) => {  
      
   return (
-    <View style={styles.content}>
-      <TouchableOpacity style={styles.imglayout}>
-        <ImageBackground resizeMode='stretch' style={{height: "100%"}} source={{ uri: 'https://static.wixstatic.com/media/913019_6d1d377f525b4b568168926a657dd13b~mv2_d_3227_3116_s_4_2.jpg/v1/fill/w_588,h_588,al_c,q_80,usm_0.66_1.00_0.01/913019_6d1d377f525b4b568168926a657dd13b~mv2_d_3227_3116_s_4_2.webp' }}>
-           <View style={{width:100, height:35, backgroundColor: Colors.sellcolor}}>
-              <View style={{flex:1, justifyContent:'center', alignItems:'center'}}> 
-                <Text>Best Seller</Text>
-              </View>
-          </View> 
-        </ImageBackground>
-      </TouchableOpacity>
-      
-      <View style={{flex:1,backgroundColor: Colors.gray}}>
-        <TouchableOpacity style={styles.contentbottom}>
-        <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
-            <Htext color={Colors.darktext} fontsize={32} fontfamily='DustWest'>ADD TO CART</Htext>
-        </View>
+    <FlatList
+      data={StaticData.Product_one} 
+      style={{flex:1}} 
+      keyExtractor={(item, index) => String(index)}
+      renderItem={({item, index}) => 
+      { 
+      return (
+        <View style={styles.content}>
+        <TouchableOpacity style={styles.imglayout}>
+          <ImageBackground resizeMode='stretch' style={{height: "100%"}} source={{ uri: item.url }}>
+             {item?.seller!=null && item?.seller!='' ?
+             <View style={{width:100, height:35, backgroundColor: Colors.sellcolor}}>
+                <View style={{flex:1, justifyContent:'center', alignItems:'center'}}> 
+                  <Text>Best Seller</Text>
+                </View>
+            </View>
+            :null} 
+          </ImageBackground>
         </TouchableOpacity>
-      </View>  
-    </View>
+        
+        <View style={{flex:1,backgroundColor: Colors.gray}}>
+          <TouchableOpacity style={styles.contentbottom}>
+          <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+              <Htext color={Colors.darktext} fontsize={32} fontfamily='DustWest'>ADD TO CART</Htext>
+          </View>
+          </TouchableOpacity>
+        </View>  
+      </View>
+      )}}  
+    />
   );
 }
 
---
+++ src/constant/StaticData.js
@@ -0,0 +1,13 @@
+export const Product_one=[
+    { id: 1,
+      url: 'https://static.wixstatic.com/media/913019_6d1d377f525b4b568168926a657dd13b~mv2_d_3227_3116_s_4_2.jpg/v1/fill/w_588,h_588,al_c,q_80,usm_0.66_1.00_0.01/913019_6d1d377f525b4b568168926a657dd13b~mv2_d_3227_3116_s_4_2.webp'
+    },
+    { id: 2,
+      seller: 'this is sell',  
+      url: 'https://static.wixstatic.com/media/913019_e041a68b0e62406184655aea26d07e1b~mv2_d_3227_3116_s_4_2.jpg/v1/fill/w_588,h_588,al_c,q_80,usm_0.66_1.00_0.01/913019_e041a68b0e62406184655aea26d07e1b~mv2_d_3227_3116_s_4_2.webp' 
+    },
+    { id: 3,
+      seller: '',
+      url: 'https://static.wixstatic.com/media/913019_6771fc17856545648d6025c0f41569bb~mv2_d_3227_3116_s_4_2.jpg/v1/fill/w_588,h_588,al_c,q_80,usm_0.66_1.00_0.01/913019_6771fc17856545648d6025c0f41569bb~mv2_d_3227_3116_s_4_2.webp' 
+    }
+  ]
