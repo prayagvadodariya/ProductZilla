3faa209f0a41e9d806a1646e64d7bfddfea47694
unknown <prayagvadodariya@gmail.com>
2021-06-16 19:28:13 +0530

    home screen design add

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -4,6 +4,8 @@ import { Text, View, StyleSheet, ScrollView } from 'react-native';
 import Maincontent from '../component/Maincontent';
 import Colors from '../constant/Colors';
 import Htext from '../component/Htext';
+import Ntext from '../component/Ntext';
+import Cbutton from '../component/Cbutton';
 import Product_Content from '../component/Product_Content';
 
 const Home = () => {
@@ -20,6 +22,18 @@ const Home = () => {
 
         <View style={{marginTop:-15}}>
           <Product_Content/>
+        </View>
+
+        <View style={{ margin:20 }}>
+          <Htext color={Colors.mainText} fontsize={35} fontfamily='DustWest'>WE CREATE PRODUCTS TO HELP YOU REACH THE FITNESS GOALS YOU NEVER KNEW YOU HAD</Htext>
+        </View>
+
+        <View style={{ marginLeft:20, marginRight:20, marginBottom:20}}>
+          <Ntext color={theme['text-custome-color']} fontsize={18} fontfamily='PTSans-Regular'>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font.</Ntext>
+        </View>
+
+        <View style={{alignSelf:'flex-start', marginLeft:20, marginBottom:15}}>
+         <Cbutton textcolor={Colors.mainText} bcolor="transparent" bwith={180} bheight={42} bordercolor={Colors.mainText}>LEARN MORE</Cbutton>
         </View>
 
       </ScrollView>
