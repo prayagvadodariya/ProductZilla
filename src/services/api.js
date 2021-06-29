22eccb8afba96d2688d6143f7f8ffcb05e74a3a5
unknown <prayagvadodariya@gmail.com>
2021-06-29 19:44:38 +0530

    api services and services hendler create

---
+++ src/services/api.js
@@ -0,0 +1,15 @@
+export const Api = "https://www.wixapis.com/stores/v1/";
+
+
+const accessToken ="enter yot token"
+
+export const headers = {  
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+};
+
+export const headers1= {
+    'Content-Type': 'application/json',
+    'Accept' : 'application/json',
+    'Authorization' :  accessToken
+}
---
+++ src/services/services_hendler.js
@@ -0,0 +1,37 @@
+import * as services  from '../services/api';
+import axios from 'axios';
+
+
+export const getuserApi = (Parameter) =>{
+    let URL = services.Api + "users?page=" + Parameter;
+     return axios.get(URL)
+      .then(response => {
+        return response.data;
+      })
+      .catch(error => {
+        return error
+      });
+}
+
+export const LoginApi = (Parameter) => {
+    let URL = services.Api + "login";
+       return axios.post(URL, Parameter)
+        .then(response => {
+          return response.data;
+        })
+        .catch(error => {
+          return error
+        });
+  }
+  
+  export const onLogoutApi = (Parameter) => {
+    let URL = services.BASE_URL + "Logout";
+    let headers = services.headers1
+       return axios.PostWithHeader(URL, Parameter, { headers })
+        .then(response => {
+          return response.data;
+        })
+        .catch(error => {
+          return error
+        });
+  }
